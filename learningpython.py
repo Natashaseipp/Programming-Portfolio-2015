7a549e82d147090fddc7f396cdbe81748f6560d5
Natasha Seipp <natashaseipp@Alveyworld-05.local>
2015-09-14 08:29:07 -0600

    This is my learning python program page thing

---
+++ learningpython.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#Learning python
+
+print "Hello World!"
+
+number = int(raw_input("Enter an integer: "))
+if number < 100:
+ print "Your number is smaller than 100"
+else:
+    print "Your number is greater than 100"
