e9bc194dbc3f860ce43ebed7682eb344031bacd7
Natasha Seipp <natashaseipp@Alveyworld-05.local>
2015-09-22 08:42:22 -0600

    more codes

---
+++ If_Else_The_Control_Flow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+number = int(raw_input("Enter a number: "))
+if number < 100:
+    print "The number is less than 100"
+else:
+    print "The number is greater than 100" 
+    
+#!/usr/bin/env python  
+x = int(raw_input("Please enter an integer: "))
+if x < 0:
+	x = 0
+		print 'Negative changed to zero'
+  elif x == 0:
+		print 'Zero'
+  elif x == 1:
+		print 'Single'
+  else:
+		print 'More
